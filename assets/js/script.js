18e62e4245e56e02258692e9d5a341552aebd744
Aegean <todddharni@gmail.com>
2023-03-25 11:08:27 -0600

    Added Date/Time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var time = document.getElementById('currentDay');
+var date = new Date();
+
+// This will take the currentDay id from the HTML Sheet and return
+// the current date and time from our date variable
+time.innerHTML = date;
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
